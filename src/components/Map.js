baeaa705c937f269639a6d786370f4aa7936f01f
Monica Salamanca <moni.salamanca@gmail.com>
2020-09-12 18:48:09 -0400

    Added polyline to track the coordinates in the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,7 @@ import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
 
-  const { state: { currentLocation } } = useContext(LocationContext);
+  const { state: { currentLocation, locations } } = useContext(LocationContext);
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
@@ -26,6 +26,9 @@ const Map = () => {
         radius={20}
         strokeColor="rgba(158, 158, 255, 1.0)"
         fillColor="rgba(158, 158, 255, 0.3)"
+      />
+      <Polyline
+        coordinates={locations.map(loc => loc.coords)}
       />
     </MapView>
   )
