14e54e3c0c2eece5bd7a2b1114b9d6d9bd41320b
Monica Salamanca <moni.salamanca@gmail.com>
2020-09-15 18:00:02 -0400

    Added save location, showlist of tracks and go to details page per track

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
-  baseURL: 'http://5cb0e3c94679.ngrok.io'
+const instance = axios.create({
+  baseURL: 'http://9ca3a7e82862.ngrok.io'
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err)
+  }
+)
+
+export default instance;
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, FlatList, TouchableOpacity } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { ListItem } from 'react-native-elements';
+import { Context as TrackContext } from '../context/TrackContext';
 
 const TrackListScreen = ({ navigation }) => {
-  return <>
-    <Text styles={{ fontSize: 48 }}>TrackListScreen</Text>
-    <Button 
-      title="Go to Track Detail"
-      onPress={() => navigation.navigate('TrackDetail')}
-    ></Button>
-  </>
+  const { state, fetchTracks } = useContext(TrackContext);
+
+  return (
+    <>
+      <NavigationEvents onWillFocus={fetchTracks} />
+      <Text styles={{ fontSize: 48 }}>TrackListScreen</Text>
+      <FlatList 
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return <TouchableOpacity onPress={() => navigation.navigate('TrackDetail', { _id: item._id })}>
+            <ListItem chevron title={item.name} />
+          </TouchableOpacity>
+        }}
+      />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({})
